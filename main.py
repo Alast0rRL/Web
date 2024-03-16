c597960bb97d7504fafb284bd33099e3ff667713
Alast0r <eshenkove@gmail.com>
2024-03-16 18:32:07 +0500

    ahahga

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return index.html
+    return render_template("index.html")
 
 @app.route('/test')
 def test():
     return 'test'
+
+@app.route('/mem')
+def mem():
+    return render_template("mem/mem.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
