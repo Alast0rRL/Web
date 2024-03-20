d279ec4324990d53bf13c495c7c0271b91eee622
Alast0r <eshenkove@gmail.com>
2024-03-21 00:41:19 +0500

    add jija

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
 def index():
-    return render_template("index.html")
+    login = "Alast0r"
+    balance = 123455
+    description = "Ну тут текст типа"
+    return render_template("index.html", description=description, login=login, balance=balance)
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        # Здесь можно выполнить логику поиска на основе запроса query
 
-@app.route('/test')
-def test():
-    return 'test'
+        # Предположим, что при успешном поиске вы хотите перенаправить пользователя на другую страницу с результатами
+        return redirect(url_for('search_results', query=query))
+
+@app.route('/search_results')
+def search_results():
+    query = request.args.get('query')
+    # Здесь вы можете использовать запрос query для вывода результатов поиска
+    return f'Результаты поиска для: {query}'
 
 @app.route('/mem')
 def mem():
