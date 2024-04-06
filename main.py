eec9c56da96bdc773add2b4477560988c35a849d
Alast0r <eshenkove@gmail.com>
2024-04-06 14:15:12 +0500

    123

--- main.py
+++ main.py
@@ -389,16 +389,14 @@ def handle_exception(e):
 
 
 
-# def application(environ, start_response):
-#     status = '200 OK'
-#     output = b'Hello World!'
+def application(environ, start_response):
+    status = '200 OK'
+    output = b'Hello World!'
 
-#     response_headers = [('Content-type', 'text/plain'),
-#                         ('Content-Length', str(len(output)))]
-#     start_response(status, response_headers)
+    response_headers = [('Content-type', 'text/plain'),('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
     
-#     return [output]
-
+    return [output]
 
 
 
