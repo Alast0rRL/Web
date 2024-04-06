e2b32bb6d8a791b5357fb002ebf369aa0744527b
Alast0r <eshenkove@gmail.com>
2024-04-06 14:05:49 +0500

    test

--- main.py
+++ main.py
@@ -396,7 +396,8 @@ def application(environ, start_response):
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
-    index()
+    
+    return [output]
 
 
 
