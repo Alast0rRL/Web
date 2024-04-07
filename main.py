83a506f81155a13bb09dbc0062fe8ce55c7ea803
Alast0r <eshenkove@gmail.com>
2024-04-07 21:28:40 +0500

    adaptive

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from werkzeug.security import check_password_hash
 from sqlalchemy import desc
 from datetime import datetime
 
-#0.1
-
-
-
+# Создание Flask-приложения
 app = Flask(__name__)
+
+# Настройка подключения к базе данных
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+# Секретный ключ для безопасности сессий и других целей
 app.config['SECRET_KEY'] = '25565e552625'
+
+# Настройки для SQLAlchemy
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 280
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
-app.secret_key = 'your_secret_key'
+
+# Создание объекта SQLAlchemy
 db = SQLAlchemy(app)
 
-
-
+# Класс модели пользователя
 class User(db.Model):
+    # Определение полей модели
     id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     login = db.Column(db.String(15), nullable=False, unique=True)
     email = db.Column(db.String(15), nullable=False, unique=True)
@@ -30,71 +34,50 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.id
 
-
+# Класс модели товара
 class Tovar(db.Model):
+    # Определение полей модели
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     description = db.Column(db.String(15), nullable=False, unique=True)
-    login = db.Column(db.String(15), nullable=False)  # Убран параметр unique=True
+    login = db.Column(db.String(15), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     full_description = db.Column(db.Text, nullable=False)
     connect= db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
-
     def __repr__(self):
         return '<Tovar %r>' % self.id
 
-
-
-
-
-
-
+# Маршрут для отображения главной страницы
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home', methods=['POST', 'GET'])
 def index():
+    # Получение списка товаров из базы данных, упорядоченного по дате добавления
     tovars = Tovar.query.order_by(Tovar.date.desc()).all()
+    
+    # Проверка наличия пользователя в сессии и передача соответствующих данных в шаблон
     if 'userLogged' not in session:
         username = "Войти"
         balance = "0"
-        email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
+        email = ""
     else:
         username = session['userLogged']
         user = User.query.filter_by(login=session['userLogged']).first()
         balance = user.balance if user else "Р"
-        email = user.email if user else ""  # Получаем email пользователя или устанавливаем пустую строку
+        email = user.email if user else ""
+        
+    # Отображение главной страницы с передачей данных о пользователях и товарах
     return render_template("index.html", tovars=tovars, username=username, balance=balance, email=email)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Маршрут для страницы "Пополнить счёт"
 @app.route('/money', methods=['GET', 'POST'])
 def money():
     if request.method == 'POST':
-        # Ваша логика для обработки POST-запроса
+        # Проверка авторизации пользователя и пополнение баланса на 1000Р
         if 'userLogged' not in session:
-            username = "Войти"
-            balance = "0"
-            email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
             flash('Вы не авторизованы')
             return redirect(url_for('login'))
         else:
-            username = session['userLogged']
             user = User.query.filter_by(login=session['userLogged']).first()
             user.balance += 1000
             try:
@@ -104,132 +87,73 @@ def money():
                 db.session.rollback()
                 print(f"Error adding tovar: {str(e)}")
                 flash('Ошибка')
-            return render_template('scam.html')
+        # Перенаправление на страницу с сообщением об успешном пополнении
+        return render_template('scam.html', username=session['userLogged'], balance=user.balance, email=user.email)
     elif request.method == 'GET':
-        # Ваша логика для обработки GET-запроса (если необходимо)
         pass
 
-
-# @app.route('/demoney', methods=['GET', 'POST'])
-# def demoney():
-#     if request.method == 'POST':
-#         # Ваша логика для обработки POST-запроса
-#         if 'userLogged' not in session:
-#             username = "Войти"
-#             balance = "0"
-#             email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
-#             flash('Вы не авторизованы')
-#             return redirect(url_for('login'))
-#         else:
-#             return render_template('scam.html')
-#     elif request.method == 'GET':
-#         # Ваша логика для обработки GET-запроса (если необходимо)
-#         pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Маршрут для страницы авторизации
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    # Проверка наличия пользователя в сессии и перенаправление на его профиль, если он уже авторизован
     if 'userLogged' in session:
         user = User.query.filter_by(login=session['userLogged']).first()
-        print("worked")
         return redirect(url_for('profile', username=session['userLogged']))
     elif request.method == 'POST':
+        # Обработка данных формы при отправке POST запроса
         username = request.form['username']
         password = request.form['password']
-        print(password)
         user = User.query.filter_by(login=username).first()
+        # Проверка введенных данных пользователя и установка соответствующей сессии
         if user and user.password == password:
             session['userLogged'] = username
-            return redirect(url_for('profile', username=username, password =password))
+            return redirect(url_for('profile', username=username))
         else:
             flash('Неверное имя пользователя или пароль')
     return render_template("login-user.html")
+
+# Маршрут для выхода из учетной записи
 @app.route('/logout')
 def logout():
-    user = User.query.filter_by(login=session['userLogged']).first()
+    # Удаление данных о пользователе из сессии при выходе
     session.pop('userLogged', None)
     return redirect(url_for('login'))
 
-
-# @app.route('/profile/<username>')
-# def profile(username):
-#     if 'userLogged' not in session or session['userLogged'] != username:
-#         abort(401)
-#     user = User.query.filter_by(login=session['userLogged']).first()
-#     return render_template('profile.html',user=user, email=user.email, password = user.password, balance=user.balance ,username=session['userLogged'])
-
-
-
-
-
+# Маршрут для отображения профиля пользователя
 @app.route('/profile')
 def profile():
-    users = User.query.all()
-    tovars = Tovar.query.all()
+    # Получение данных о пользователе из сессии и отображение их на странице профиля
     user = User.query.filter_by(login=session['userLogged']).first()
-    if user:
-        email = user.email
-        password = user.password
-        balance = user.balance
-    else:
-        email = ""  # установите значение по умолчанию для email, если пользователь не найден
-        password = ""  # установите значение по умолчанию для password, если пользователь не найден
-        balance = ""  # установите значение по умолчанию для balance, если пользователь не найден
-    return render_template('profile.html', user=user, email=email, password=password, balance=balance, username=session['userLogged'])
+    email = user.email if user else ""
+    balance = user.balance if user else ""
+    return render_template('profile.html', user=user, email=email, balance=balance, username=session['userLogged'])
 
-
-
-
-
+# Маршрут для отображения информации о товаре
 @app.route('/tovar', methods=['POST'])
 def tovar_details():
+    # Получение информации о выбранном товаре и отображение его на отдельной странице
     tovars = Tovar.query.order_by(Tovar.date.desc()).all()
     if 'userLogged' not in session:
         username = "Войти"
         balance = ""
-        email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
+        email = ""
     else:
         username = session['userLogged']
         user = User.query.filter_by(login=session['userLogged']).first()
-        balance = user.balance if user else "Р"
-        email = user.email if user else ""  # Получаем email пользователя или устанавливаем пустую строку
+        balance = user.balance if user else ""
+        email = user.email if user else ""
     if request.method == 'POST':
         tovar_id = request.form['tovar_id']
-        tovar = Tovar.query.get(tovar_id)  # Получаем информацию о товаре по его идентификатору из базы данных
+        tovar = Tovar.query.get(tovar_id)
         if tovar:
             return render_template('tovar.html', tovar=tovar, tovars=tovars, username=username, balance=balance, email=email)
         else:
             return "Товар не найден"
 
-
-
-
-
+# Маршрут для создания нового товара
 @app.route('/create-tovar', methods=['POST', 'GET'])
 def create_tovar():
+    # Обработка данных формы при создании нового товара
     if request.method == 'POST':
         description = request.form['description']
         login = session['userLogged']
@@ -239,12 +163,10 @@ def create_tovar():
             flash('Товар добавлен')
             e="Неправильный формат цены"
             return render_template('error.html', error=str(e)), 500
-        full_description = request.form['full_description']  # Получаем полное описание товара из формы
+        full_description = request.form['full_description']
         connect = request.form['connect']
         date = datetime.utcnow()
-
         tovar = Tovar(description=description, login=login, price=price, full_description=full_description,connect=connect, date=date)
-
         try:
             db.session.add(tovar)
             db.session.commit()
@@ -258,16 +180,18 @@ def create_tovar():
         if 'userLogged' not in session:
             username = "Войти"
             balance = "0"
-            email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
+            email = ""
         else:
             username = session['userLogged']
             user = User.query.filter_by(login=session['userLogged']).first()
-            balance = user.balance if user else "Р"
-            email = user.email if user else ""  # Получаем email пользователя или устанавливаем пустую строку
+            balance = user.balance if user else ""
+            email = user.email if user else ""
         return render_template("create-tovar.html",username=username, balance=balance, email=email)
 
+# Маршрут для создания нового пользователя
 @app.route('/create-user', methods=['POST', 'GET'])
 def create_user():
+    # Обработка данных формы при создании нового пользователя
     if request.method == 'POST':
         with app.app_context():
             last_user = User.query.order_by(desc(User.id)).first()
@@ -275,7 +199,6 @@ def create_user():
                 id = last_user.id + 1
             else:
                 id = 1
-            
             login = request.form['login']
             email = request.form['email']
             password = request.form['password']
@@ -293,35 +216,64 @@ def create_user():
                 flash(f"Произошла ошибка при добавлении пользователя: {str(e)}")
                 return f"Произошла ошибка при добавлении пользователя: {str(e)}"
     else:
-        return render_template("create-user.html")
+        if 'userLogged' not in session:
+            username = "Войти"
+            balance = "0"
+            email = ""
+        else:
+            username = session['userLogged']
+            user = User.query.filter_by(login=session['userLogged']).first()
+            balance = user.balance if user else ""
+            email = user.email if user else ""
+        return render_template("create-user.html", username=username, balance=balance, email=email)
 
+# Маршрут для поиска товаров
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
         query = request.form['query']
         return redirect(url_for('search_results', query=query))
 
+# Маршрут для отображения результатов поиска
 @app.route('/search_results')
 def search_results():
     query = request.args.get('query')
-    if query.lower() == "mem":  # Исправлено на использование lower()
+    if query.lower() == "mem":
         return render_template("mem/mem.html")
+    elif query.lower() == "test":
+        tovars = Tovar.query.order_by(Tovar.date.desc()).all()
+    # Проверка наличия пользователя в сессии и передача соответствующих данных в шаблон
+        if 'userLogged' not in session:
+            username = "Войти"
+            balance = "0"
+            email = ""
+        else:
+            username = session['userLogged']
+            user = User.query.filter_by(login=session['userLogged']).first()
+            balance = user.balance if user else "Р"
+            email = user.email if user else ""
+        # Отображение главной страницы с передачей данных о пользователях и товарах
+        return render_template("test.html", tovars=tovars, username=username, balance=balance, email=email)
+
+
+
     elif query.lower() == "ananas":
         return render_template("ananas.html")
     else:
-        return render_template("not_found.html")  # Добавлено сообщение о том, что запрос не найден
+        return render_template("not_found.html")
 
+# Маршруты для отображения страниц создания пользователя и товара
 @app.route('/create-user-page', methods=['GET', 'POST'])
 def create_user_page():
     if 'userLogged' not in session:
             username = "Войти"
             balance = "0"
-            email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
+            email = ""
     else:
         username = session['userLogged']
         user = User.query.filter_by(login=session['userLogged']).first()
-        balance = user.balance if user else "Р"
-        email = user.email if user else ""  # Получаем email пользователя или устанавливаем пустую строку
+        balance = user.balance if user else ""
+        email = user.email if user else ""
     return render_template("create-user.html",username=username, balance=balance, email=email)
 
 @app.route('/create-tovar-page', methods=['GET', 'POST'])
@@ -329,67 +281,29 @@ def create_tovar_page():
     if 'userLogged' not in session:
             username = "Войти"
             balance = "0"
-            email = ""  # Добавляем пустую строку для email, чтобы избежать ошибки
+            email = ""
     else:
         username = session['userLogged']
         user = User.query.filter_by(login=session['userLogged']).first()
-        balance = user.balance if user else "Р"
-        email = user.email if user else ""  # Получаем email пользователя или устанавливаем пустую строку
+        balance = user.balance if user else ""
+        email = user.email if user else ""
     return render_template("create-tovar.html",username=username, balance=balance, email=email)
 
-
+# Маршрут для страницы помощи
 @app.route('/help')
 def help():
     return redirect("https://i.pinimg.com/736x/94/95/d9/9495d94e62132fb17ae2b9ddff0690bf.jpg")
 
-
-
-
-
-
-
-
-
+# Обработчик ошибок для HTTP ошибки 404
 @app.errorhandler(404)
 def handle_exception(e):
     return render_template('error.html', error=str("Страница не найдена")), 500
 
-
-
+# Обработчик ошибок для других исключений
 @app.errorhandler(Exception)
 def handle_exception(e):
     return render_template('error.html', error=str(e)), 500
 
-
-
-# @app.errorhandler(userLogged)
-# def handle_exception(e):
-#     return render_template('error.html', error=str("Страница не найдена")), 500
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def application(environ, start_response):
-#     status = '200 OK'
-#     output = b'Hello World!'
-
-#     response_headers = [('Content-type', 'text/plain'),('Content-Length', str(len(output)))]
-#     start_response(status, response_headers)
-    
-#     return [output]
-
-
-
+# Запуск приложения
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
